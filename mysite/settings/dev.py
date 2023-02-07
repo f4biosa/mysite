18832ed2506479fbec874180a00dbdd03460ecd0
Fabio Santos <f4bio.sa@gmail.com>
2023-02-07 10:12:50 -0400

    initial commit

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-mrh8fokad+s^d=fn!a5=$n!1l2_l8)+5g$+zk38+#0+8tt=#kw"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
